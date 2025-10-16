be0d7f66d7c8c3bbdf471d7287ca4664b2b282ef
Jordy <jordyreins@gmail.com>
2025-10-16 19:27:38 +0200

    1.21.10 ADD: nether portal break

--- paper/src/main/java/nl/gjorgdy/pl3xMarkers/NetherPortalListener.java
+++ paper/src/main/java/nl/gjorgdy/pl3xMarkers/NetherPortalListener.java
@@ -7,6 +7,7 @@ import org.bukkit.block.BlockType;
 import org.bukkit.block.data.Orientable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 
 public class NetherPortalListener implements Listener {
@@ -14,20 +15,52 @@ public class NetherPortalListener implements Listener {
     @EventHandler
     public void onPortalTeleport(PlayerPortalEvent event) {
         var fromBlockType = event.getFrom().getBlock().getType().asBlockType();
-        var toBlockType = event.getTo().getBlock().getType().asBlockType();
         if (fromBlockType == BlockType.NETHER_PORTAL) {
             onNetherPortalTeleport(event.getFrom());
-        } else if (toBlockType == BlockType.NETHER_PORTAL) {
-            onNetherPortalTeleport(event.getTo());
         } else if (fromBlockType == BlockType.END_PORTAL) {
             onEndPortalTeleport(event.getFrom());
         }
     }
 
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        var blockType = event.getBlock().getType().asBlockType();
+        var location = event.getBlock().getLocation();
+        if (blockType == BlockType.OBSIDIAN) {
+            // check if there is a nether portal nearby
+            var nearby = location.clone();
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        nearby.set(location.getBlockX() + x, location.getBlockY() + y, location.getBlockZ() + z);
+                        if (nearby.getBlock().getType().asBlockType() == BlockType.NETHER_PORTAL) {
+                            var center = getNetherPortalCenter(nearby);
+                            onNetherPortalBreak(center);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        if (blockType == BlockType.NETHER_PORTAL) {
+            var center = getNetherPortalCenter(location);
+            onNetherPortalBreak(center);
+        }
+    }
+
+    private void onNetherPortalBreak(Location location) {
+        var center = getNetherPortalCenter(location);
+        Pl3xMarkersCore.api().removeNetherPortalIconMarker(location.getWorld().getName(), center.getBlockX(), center.getBlockZ());
+    }
+
     private void onNetherPortalTeleport(Location location) {
+        var center = getNetherPortalCenter(location);
+        Pl3xMarkersCore.api().addNetherPortalIconMarker(location.getWorld().getName(), center.getBlockX(), center.getBlockZ());
+    }
+
+    private Location getNetherPortalCenter(Location location) {
         if (location.getBlock().getBlockData() instanceof Orientable orientable) {
             boolean isX = orientable.getAxis() == Axis.X;
-            System.out.println("Nether portal axis: " + orientable.getAxis());
             var lowCorner = location.clone();
             while (lowCorner.getBlock().getType().asBlockType() == BlockType.NETHER_PORTAL) {
                 lowCorner = lowCorner.add(isX ? -1 : 0, 0, isX ? 0 : -1);
@@ -40,10 +73,9 @@ public class NetherPortalListener implements Listener {
             int deltaZ = Math.abs(highCorner.getBlockZ() - lowCorner.getBlockZ());
             int centerX = lowCorner.getBlockX() + deltaX / 2;
             int centerZ = lowCorner.getBlockZ() + deltaZ / 2;
-            Location center = location.clone().set(centerX, 0, centerZ);
-            System.out.println("Nether portal at " + center + " size " + deltaX + "x" + deltaZ);
-            Pl3xMarkersCore.api().addNetherPortalIconMarker(location.getWorld().getName(), center.getBlockX(), center.getBlockZ());
+            return location.clone().set(centerX, 0, centerZ);
         }
+        return location;
     }
 
     private void onEndPortalTeleport(Location location) {
