b68e672d5d75538ad270892f5cd966f372b7ae50
Gjorgdy <jordy.reins@proton.me>
2025-10-31 22:19:37 +0100

    1.21.10-opac ADD: stable load logic

--- fabric/src/main/java/nl/gjorgdy/pl3xmarkers/fabric/compat/layers/OPACAreaMarkerLayer.java
+++ fabric/src/main/java/nl/gjorgdy/pl3xmarkers/fabric/compat/layers/OPACAreaMarkerLayer.java
@@ -1,40 +1,41 @@
 package nl.gjorgdy.pl3xmarkers.fabric.compat.layers;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.pl3x.map.core.world.World;
+import nl.gjorgdy.pl3xmarkers.core.Pl3xMarkersCore;
 import nl.gjorgdy.pl3xmarkers.core.layers.primitive.MarkerLayer;
 import nl.gjorgdy.pl3xmarkers.core.markers.AreaMarkerBuilder;
 import nl.gjorgdy.pl3xmarkers.core.markers.MarkerBuilder;
 import nl.gjorgdy.pl3xmarkers.core.registries.Layers;
+import nl.gjorgdy.pl3xmarkers.fabric.FabricMarkersConfig;
 import nl.gjorgdy.pl3xmarkers.fabric.Pl3xMarkersFabric;
 import nl.gjorgdy.pl3xmarkers.fabric.compat.OpacChunk;
 import nl.gjorgdy.pl3xmarkers.fabric.compat.OpacHandler;
 import nl.gjorgdy.pl3xmarkers.fabric.compat.OpacListener;
 import org.jetbrains.annotations.NotNull;
 import xaero.pac.common.server.api.OpenPACServerAPI;
-import xaero.pac.common.server.claims.ServerClaimsManager;
 
 public class OPACAreaMarkerLayer extends MarkerLayer {
 
     public OPACAreaMarkerLayer(@NotNull World world) {
-        super(Layers.Keys.OPAC, Layers.Labels.OPAC, world, 99);
+        super(Layers.Keys.OPAC, Layers.Labels.OPAC, world, FabricMarkersConfig.OPAC_MARKERS_PRIORITY);
     }
 
     @Override
     public void load() {
-        ServerLifecycleEvents.ServerStarted fun = (server) -> {
-            var chunks = OpacHandler.load(server, worldIdentifier);
-            chunks.forEach(this::addChunk);
-        };
-		// or directly if server already started
-		if (Pl3xMarkersFabric.isOpacLoaded(getServer())) {
-			fun.onServerStarted(getServer());
-		// run on server start
-		} else {
-			ServerLifecycleEvents.SERVER_STARTED.register(fun);
-		}
+		Pl3xMarkersCore.runParallel(() -> {
+			var server = getServer();
+			while (!Pl3xMarkersFabric.isOpacLoaded(server)) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					return;
+				}
+			}
+			OpacHandler.load(server, worldIdentifier)
+				.forEach(this::addChunk);
+		});
 		// register listener for claim changes
 		OpenPACServerAPI.get(getServer())
 				.getServerClaimsManager()
